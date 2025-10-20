dae565d5d79aa4df5392aaac7b8c7523e732e823
cburrows404 <cburrow5@kent.edu>
2025-10-20 17:51:21 -0400

    updated code

--- consumer.cpp
+++ consumer.cpp
@@ -8,3 +8,33 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <memoryapi.h>
+int main(int argc, char *argv[]) {
+    int fileDescription;
+    char * shmpath;
+    struct sharedMem *consumerMem;
+    shmpath = argv[1];
+    fileDescription = shm_open(shmpath, O_RDWR, 0);
+
+
+
+    //Truncate it to the size of the struct, ensures extra is not used
+    ftruncate(fd, sizeof(*consumerMem));
+
+    //Maps to local memory space
+    consumerMem = static_cast<sharedMem*>(mmap(NULL, sizeof(*consumerMem), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0));
+
+    for (int i = 4; i >= 0; --i) {
+        sem_wait(&(consumerMem->full));
+        sem_wait(&(consumerMem->mutex));
+
+        int itemToConsume;
+        itemToConsume = consumerMem->table[consumerMem->out];
+        std::cout << "item consumed: " <<  itemToConsume << std::endl;
+        consumerMem->out = (consumerMem->out + 1) % TABLE_SIZE;
+
+        sem_post(&(consumerMem->mutex));
+        sem_post(&(consumerMem->empty));
+    }
+    shm_unlink(shmpath);
+    return 0;
+}
--- producer.cpp
+++ producer.cpp
@@ -22,6 +22,33 @@
 
     };
 int main(int argc, char *argv[]) {
-    
+    int fileDescription;
+    char * shmpath;
+    struct sharedMem *producerMem;
+    shmpath = argv[1];
+    fileDescription=shm_open(shmpath, O_CREAT | O_RDWR, 0600);
+
+    ftruncate(fd, sizeof(*producerMem))
+    producerMem = static_cast<sharedMem*>(CreateFileMapping(NULL, NULL, PAGE_EXECUTE_READWRITE, sizeof(*producerMem), fileDescription, 0));
+
+    sem_init(&(producerMem->mutex),1,1);
+    sem_init(&(producerMem->full),1,0);
+    sem_init(&(producerMem->empty),1,TABLE_SIZE);
+    producerMem->in, producerMem->out=0;
+    for(int i = 4; i >= 0; --i) {
+        sem_wait(&producerMem->empty);
+        sem_wait(&producerMem->mutex);
+
+        int produced = rand();
+        std::cout << "item produced: " << produced << std::endl;
+
+        producerMem->table[producerMem->in] = produced;
+        producerMem->in = (producerMem->in + 1) % TABLE_SIZE;
+
+        sem_post(&producerMem->mutex);
+        sem_post(&producerMem->full);
+    }
+    shm_unlink(shmpath);
+    return 0;
 }
 
